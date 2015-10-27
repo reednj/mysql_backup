e79fa80af284b9be8f74406c873d5d342504b129
Nathan Reed <reednj@gmail.com>
2015-10-27 09:34:38 +0800

    new extensions file with some methods to make it easier to load

---
+++ shared/extensions.rb
@@ -0,0 +1,27 @@
+
+
+def try_load_file(path)
+	begin
+		load path
+		return path
+	rescue LoadError => e
+		return nil
+	end
+end
+
+def try_load(paths)
+	Array.from(paths).each do |path|
+		result = try_load_file path
+		return result if !result.nil?
+	end
+
+	return nil
+end
+
+class Array
+	def self.from(a)
+		return a if a.is_a? Array
+		return [] if a.nil?
+		return [a]
+	end
+end
